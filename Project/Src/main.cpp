754aaca019bbb72ea6de6f262e6b72c1bbd3c989
Reid <reidyaro@gmail.com>
2016-12-08 16:48:05 +0100

    Add new Object files.

--- Project/Src/main.cpp
+++ Project/Src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
     Quad q(Random::NextUInt64(), Vector2(0, 5), Vector2(5, 5), Vector2(5, 0), Vector2());
 
     std::ofstream obj;
-    q.BuildTerrain (obj, Setting::GetInstance());
+    q.BuildTerrain (obj, Setting::GetInstance(q.A()));
 
 	std::cout << t.A().X() << " " << t.A().Y() << " ; " << t.B().X() << " " << t.B().Y() << " ; " << t.C().X() << " " << t.C().Y() << std::endl;
 
