ea2bb2520b4d7b369019eeabfc9758f4d78a0aae
Vizepi <joseph.kieffer@edu.univ-fcomte.fr>
2016-12-08 17:02:04 +0100

    Finished interpolation

--- Project/Include/Setting.h
+++ Project/Include/Setting.h
@@ -39,9 +39,24 @@ struct Setting
 	static inline BuildingSetting	GetInstance(const Vector2 & position)
 	{
 		BuildingSetting instance;
+		instance.Height.Min = 0;
+		instance.Height.Max = 0;
+		instance.Size.Min = 0;
+		instance.Size.Max = 0;
 		for (uint64_t nPoint = 0; nPoint < InfluencePointsCount; ++nPoint)
 		{
-			double x = 0.0;
+			double radius = InfluencePoints[nPoint].radius;
+			double x = radius ? (position - InfluencePoints[nPoint].position).Length() / radius : 1.0;
+			if (x > 1.0)
+			{
+				continue;
+			}
+			double y = (1.0 - x*x);
+			y *= y*y;
+			instance.Height.Min += InfluencePoints[nPoint].setting.Height.Min * y;
+			instance.Height.Max += InfluencePoints[nPoint].setting.Height.Max * y;
+			instance.Size.Min += InfluencePoints[nPoint].setting.Size.Min * y;
+			instance.Size.Max += InfluencePoints[nPoint].setting.Size.Max * y;
 		}
 		return instance;
 	}
